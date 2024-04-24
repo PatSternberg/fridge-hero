ab08b918b62310dccdcf6dd39e2dc1ceda250f2d
kevin eboda <kevineboda@gmail.com>
2024-04-24 11:54:42 +0100

    created user view & updated urls.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,4 +1,7 @@
 # file: views.py
+from django.http import HttpResponse
+from pymongo import MongoClient
+
 
 from django.shortcuts import render
 
@@ -6,7 +9,25 @@ from django.shortcuts import render
 
 # Example of importing the database connection logic and creating
 # a database connection
-from .utils import get_db_handle
+#from .utils import get_db_handle
+
+
+def user_view(request):                 #http request from client
+    # connect to mongo db
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['fridge_hero']
+    collection = db['users']
+
+    # testing if i can insert a user to the db
+    user_data = {
+        'id': '011',
+        'username': 'kevin',
+        'email': 'kevin@gmail.com',
+        'password': 'pass123'
+    }
+    collection.insert_one(user_data)
+
+    return HttpResponse('user inserted succesfully')
 
 def my_mongo_view():
     # Your MongoDB connection details
